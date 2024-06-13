4908604653a55e5ec2b6977dd7d19123d65ee146
edunm1981 <161252950+edunm1981@users.noreply.github.com>
2024-06-12 21:36:25 -0600

    noveno comentario

--- main.js
+++ main.js
@@ -17,4 +17,41 @@ const TIPO_CAMBIO_QUETZAL_COLONES = 68.07;
 const TIPO_CAMBIO_MEXICO_COLONES = 425.15;
 const TIPO_CAMBIO_HONDURAS_COLONES = 21.44;
 
+let monedaSeleccionada = 'USD';
+
+liEUR.addEventListener('click', () => {
+    btnDropdown.innerText = 'Euro';  
+    btnMonedaColones.innerText = 'Euros a colones';  
+    btnColonesMoneda.innerText = 'Colones a euros';  
+    monedaSeleccionada = 'EUR';
+});
+
+liUSD.addEventListener('click', () => {
+    btnDropdown.innerText = 'Dolar'; 
+    btnMonedaColones.innerText = 'Dólares a colones'; 
+    btnColonesMoneda.innerText = 'Colones a dólares'; 
+    monedaSeleccionada = 'USD';
+});
+
+liQ.addEventListener('click', () => {
+    btnDropdown.innerText = 'Quetzal'; 
+    btnMonedaColones.innerText = 'Quetzales a colones'; 
+    btnColonesMoneda.innerText = 'Colones a Quetzales'; 
+    monedaSeleccionada = 'Q';
+});
+
+liMXN.addEventListener('click', () => {
+    btnDropdown.innerText = 'Mexico'; 
+    btnMonedaColones.innerText = 'Pesos a colones'; 
+    btnColonesMoneda.innerText = 'Colones a Pesos'; 
+    monedaSeleccionada = 'MXN';
+});
+
+liLPS.addEventListener('click', () => {
+    btnDropdown.innerText = 'Honduras'; 
+    btnMonedaColones.innerText = 'Lempiras a colones'; 
+    btnColonesMoneda.innerText = 'Colones a Lempiras'; 
+    monedaSeleccionada = 'LPS';
+});
+
 
