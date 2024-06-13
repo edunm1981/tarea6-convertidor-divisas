b15526e05d27232140f8b7008be85b8b311c1b13
edunm1981 <161252950+edunm1981@users.noreply.github.com>
2024-06-12 21:27:47 -0600

    sexto comentario

---
+++ main.js
@@ -0,0 +1,8 @@
+const btnDropdown = document.querySelector("button[data-bs-toggle='dropdown']");
+const btnMonedaColones = document.querySelector("#moneda-a-colones"); 
+const btnColonesMoneda = document.getElementById("colones-a-moneda"); 
+
+const inputValorMoneda = document.getElementById("valor-moneda");
+const inputValorColones = document.getElementById("valor-colones");
+
+
